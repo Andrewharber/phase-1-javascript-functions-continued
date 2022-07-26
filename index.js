a6dd770c287128693e352260ed5d57023b27ded7
Andrewharber <harber.andrew@ymail.com>
2022-07-26 10:27:58 -0400

    commit tree 56d598a43ac62f55f9dcd693750683525b806a62

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity ='roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+const mondayWork = function(work = 'go to the office') {
+    return `This Monday, I will ${work}.`
+}
+mondayWork('work from home');
+
+function wrapAdjective(wrapAdjective = "*"){
+    const inner = function(adjective = "a dedicated programer") {
+        return `You are ${wrapAdjective}${adjective}${wrapAdjective}!`;
+    };
+    return inner;
+}
